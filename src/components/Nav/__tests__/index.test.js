92febed03a65fed7a0c91978f3200f0ae8e71e85
Nathan Chow <emailme@nathanchow.ca>
2021-05-06 15:33:30 -0400

    update gallery

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,6 +3,12 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
@@ -35,4 +41,5 @@ describe('links are visible', () => {
         // Arrange
         // Assert
     });
+
 })
