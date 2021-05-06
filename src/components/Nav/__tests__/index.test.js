624e0377ea71d91b606feae9b5c52daff24d021a
Nathan Chow <emailme@nathanchow.ca>
2021-05-06 16:47:47 -0400

    update all

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -6,8 +6,10 @@ import Nav from '..';
 const categories = [
     { name: 'portraits', description: 'Portraits of people in my life' }
   ]
-const mockCurrentCategory = jest.fn();
-const mockSetCurrentCategory = jest.fn();
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+  const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -41,5 +43,15 @@ describe('links are visible', () => {
         // Arrange
         // Assert
     });
+
+    it('renders', () => {
+        render(<Nav
+          categories={categories}
+          setCurrentCategory={mockSetCurrentCategory}
+          currentCategory={mockCurrentCategory}
+          contactSelected={mockContactSelected}
+          setContactSelected={mockSetContactSelected}
+        />);
+      })
 
 })
